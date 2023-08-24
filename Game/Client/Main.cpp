37659d02bb91ff0ab45de2c62128de4b3d89d9db
heonwoochoo <cngjsdn9@gmail.com>
2023-08-24 13:49:37 +0900

    quad demo

--- Game/Client/Main.cpp
+++ Game/Client/Main.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "Main.h"
 #include "Game.h"
-#include "TriangleDemo.h"
+#include "QuadDemo.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -13,7 +13,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.5f, 0.5f, 0.5f, 0.5f);
-	desc.app = make_shared<TriangleDemo>();
+	desc.app = make_shared<QuadDemo>();
 
 	GAME->Run(desc);
 
